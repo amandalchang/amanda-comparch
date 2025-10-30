6e40b7e760431ae788f7975fae67e9ebbfad0ae8
Amanda Chang <achang2@olin.edu>
2025-10-30 03:21:51 -0400

    adding three simultaneous game of life implementation

--- miniproject3/game_of_life.sv
+++ miniproject3/game_of_life.sv
@@ -1,40 +1,24 @@
-module game_of_life;
-
-    logic current_grid [0:7][0:7]; // 8x8 grid of bools
-    logic next_grid [0:7][0:7];
-    logic [2:0] neighbor_count;
-
-    initial begin    
-    // initialize grid to have 0s
-    for (int i=0; i<8; i++)
-        for (int j=0; j<8; j++)
-            current_grid[i][j] = 0;
-    // put pattern into initial grid
-    current_grid[2][3] = 1'b1;
-    current_grid[2][4] = 1'b1;
-    current_grid[2][5] = 1'b1;
-    current_grid[3][5] = 1'b1;
-    current_grid[4][3] = 1'b1;
-    current_grid[4][4] = 1'b1;
-    current_grid[4][5] = 1'b1;
-    neighbor_count = 0;
-
-    // print initial grid
-    $display("\nInitial Grid:");
-    for (int i=0; i<8; i++) begin
-        for (int j=0; j<8; j++)
-            $write("%0d ", current_grid[i][j]);
-        $write("\n");
-    end
-    end
+module game_of_life #(
+    parameter INIT_FILE = ""
+)(
+    input logic clk,
+    input logic time_to_calc_frame,
+    output logic [63:0] output_array
+);
+    logic [0:7] current_grid [0:7]; // partially packed 8x8 grid of bools
+    logic [0:7] next_grid [0:7];
+    logic [2:0] neighbor_count = 2'b0;
 
     initial begin
-    int num_steps = 5;
-    for (int step = 0; step < num_steps; step++) begin
-        #1000000; // wait before next step (this is broken)
+
+    if (INIT_FILE != "") begin
+            $readmemb(INIT_FILE, current_grid);
+        end
+    end
+
+    always_ff @(posedge time_to_calc_frame) begin
         for (int i=0; i<8; i++) begin
             for (int j=0; j<8; j++) begin
-
                 // Neighbor counting
                 for (int a = -1; a<2; a++) begin
                     for (int b = -1; b<2; b++) begin
@@ -45,32 +29,27 @@ module game_of_life;
                 end
 
                 // Create new grid based on number of neighbors
-                if (current_grid[i][j] == 1) begin
+                if (current_grid[i][j] == 1) begin // if the current pixel is currently alive
+                    // if the neighbor count is 2 or 3 then it should stay alive
+                    // otherwise it dies
                     next_grid[i][j] = (neighbor_count == 2 || neighbor_count == 3);
                 end
-                else next_grid[i][j] = (neighbor_count == 3);
+                // if the current pixel is dead & the neighbor count is 3, then alive
+                else next_grid[i][j] = (neighbor_count == 3); 
 
                 neighbor_count = 0;
             end
         end
 
-        // print next grid
-        $display("\nNext Grid:");
-        for (int i=0; i<8; i++) begin
+        // Flatten next grid and put it in output_array
+        for (int i=0; i<8; i++)
             for (int j=0; j<8; j++)
-                $write("%0d ", next_grid[i][j]);
-            $write("\n");
-        end
+                output_array[i*8 + j] = next_grid[i][j];
 
         // Move the new grid into the current grid
         for (int i=0; i<8; i++)
             for (int j=0; j<8; j++)
                 current_grid[i][j] = next_grid[i][j];
     end
-    $finish;
-    end
 
 endmodule
-
-// iverilog -g2012 -o game_of_life.vvp game_of_life.sv
-// vvp game_of_life.vvp
---
+++ miniproject3/top.sv
@@ -0,0 +1,118 @@
+// `include "memory.sv"
+`include "ws2812b.sv"
+`include "controller.sv"
+`include "game_of_life.sv"
+
+// led_matrix top level module
+
+module top(
+    input logic     clk, 
+    input logic     SW, 
+    input logic     BOOT, 
+    output logic    _48b, 
+    output logic    _45a, 
+    output logic    RGB_R 
+);
+
+    logic [7:0] red_data;
+    logic [7:0] green_data;
+    logic [7:0] blue_data;
+
+    logic [5:0] pixel;
+    logic [4:0] frame;
+    logic [10:0] address;
+    logic [5:0] pixel_count = 6'b0;
+    logic [63:0] green_output_grid;
+    logic [63:0] red_output_grid;
+    logic [63:0] blue_output_grid;
+
+    logic [23:0] shift_reg = 24'd0;
+    logic debug;
+    logic load_sreg;
+    logic transmit_pixel;
+    logic shift;
+    logic ws2812b_out;
+    logic time_to_calc_frame;
+    logic [4:0] slow_down_count;
+    logic [63:0] test_array = {3'b111, 61'b0};
+
+    assign address = { frame, pixel };
+
+    // Instance the green game of life
+    game_of_life #(
+        .INIT_FILE          ("initial/greeninit.txt")
+    ) u1 (
+        .clk                (clk),
+        .time_to_calc_frame (time_to_calc_frame),
+        .output_array       (green_output_grid)      
+    );
+
+    // Instance the red game of life
+    game_of_life #(
+        .INIT_FILE          ("initial/redinit.txt")
+    ) u2 (
+        .clk                (clk),
+        .time_to_calc_frame (time_to_calc_frame),
+        .output_array       (red_output_grid)
+    );
+
+    // Instance the blue game of life
+    game_of_life #(
+        .INIT_FILE          ("initial/blueinit.txt")
+    ) u3 (
+        .clk                (clk),
+        .time_to_calc_frame (time_to_calc_frame),
+        .output_array       (blue_output_grid)
+    );
+
+    // Instance the WS2812B output driver
+    ws2812b u4 (
+        .clk            (clk), 
+        .serial_in      (shift_reg[23]), 
+        .transmit       (transmit_pixel), 
+        .ws2812b_out    (ws2812b_out), 
+        .shift          (shift)
+    );
+
+    // Instance the controller
+    controller u5 (
+        .clk            (clk), 
+        .load_sreg      (load_sreg), 
+        .transmit_pixel (transmit_pixel), 
+        .pixel          (pixel), 
+        .frame          (frame)
+    );
+
+
+    always_ff @(posedge clk) begin
+        if (load_sreg) begin // if it's time to load one pixel
+            green_data = green_output_grid[pixel_count] ? 8'b10010000 : 8'hFF;
+            blue_data = blue_output_grid[pixel_count] ? 8'b10010000 : 8'hFF;
+            red_data = red_output_grid[pixel_count] ? 8'b10010000 : 8'hFF;
+            pixel_count = pixel_count + 1; // add one to pixel count
+            unique case ({ SW, BOOT })
+                2'b00:
+                    shift_reg <= { green_data, 16'd0 };
+                2'b01:
+                    shift_reg <= { 8'd0, red_data, 8'd0 };
+                2'b10:
+                    shift_reg <= { 16'd0, blue_data };
+                2'b11:
+                    shift_reg <= { green_data, red_data, blue_data };
+            endcase
+            if (pixel_count == 0) begin // if pixel count is 65 or 0
+                time_to_calc_frame = 1'b1; // full frame has been shifted in
+                debug = ~debug;
+            end
+            else time_to_calc_frame = 1'b0; // otherwise it's not time to calculate the state/keep old state
+        end
+        else if (shift) begin
+            shift_reg <= { shift_reg[22:0], 1'b0 };
+        end
+    end
+
+    assign RGB_R = ~debug;
+    assign _48b = ws2812b_out;
+    assign _45a = ~ws2812b_out;
+
+endmodule
